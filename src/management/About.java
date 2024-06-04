6e4cf25314078f382fa4dc45d2447c067b6b9559
Chaitya Shah <102247651+Chaitya54@users.noreply.github.com>
2024-06-04 20:32:21 +0530

    Update About.java

--- src/management/About.java
+++ src/management/About.java
@@ -22,17 +22,17 @@ public class About extends JFrame {
         heading.setFont(new Font("Tahoma", Font.BOLD, 30));
         add(heading);
         
-        JLabel name = new JLabel("<html>Developed By:<br/>Jay Chotalia<br/>Naman Chheda<br/>Chaitya Shah</html>");
+        JLabel name = new JLabel("<html>Developed By:<br/>Chaitya Shah</html>");
         name.setBounds(20, 30, 550, 400);
         name.setFont(new Font("Tahoma", Font.PLAIN, 25));
         add(name);
         
-        JLabel rollno = new JLabel("<html>SAP ID: <br/>60009210027<br/>60009210038<br/>60009210054</html>");
+        JLabel rollno = new JLabel("<html>SAP ID: <br/>60009210054</html>");
         rollno.setBounds(225, 30, 550, 400);
         rollno.setFont(new Font("Tahoma", Font.PLAIN, 25));
         add(rollno);
         
-        JLabel contact = new JLabel("<html>Contact: <br/>jaychotalia11@gmail.com<br/>namanchheda3@gmail.com<br/>chaityashah540@gmail.com</html>");
+        JLabel contact = new JLabel("<html>Contact: <br/>chaityashah540@gmail.com</html>");
         contact.setBounds(70, 275, 550, 200);
         contact.setFont(new Font("Tahoma", Font.PLAIN, 20));
         add(contact);
